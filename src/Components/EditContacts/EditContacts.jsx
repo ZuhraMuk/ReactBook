45cd2f1367c5811becfb9d3f7e81d5842729c8ef
ZuhraMuk <zmu3842@gmail.com>
2022-10-21 00:24:19 +0300

    first commit

---
+++ src/Components/EditContacts/EditContacts.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./EditContacts.css";
+
+const EditContacts = props => {
+  const [name, setName] = useState(props.editContact.name);
+  const [email, setEmail] = useState(props.editContact.email);
+  const [pic, setPic] = useState(props.editContact.pic);
+
+  function handleSaveClick() {
+    let contact = { ...props.editContact };
+    contact.name = name;
+    contact.email = email;
+    contact.pic = pic;
+
+    props.handleSaveEditedContact(contact);
+    setName("");
+    setEmail("");
+    setPic("");
+  }
+
+  return (
+    <div className="inpAdd">
+      <input
+        onChange={e => setName(e.target.value)}
+        type={"text"}
+        placeholder={"Имя"}
+        value={name}
+      />
+      <input
+        onChange={e => setEmail(e.target.value)}
+        type={"email"}
+        placeholder={"email"}
+        value={email}
+      />
+      <input
+        onChange={e => setPic(e.target.value)}
+        type={"url"}
+        placeholder={"url"}
+        value={pic}
+      />
+      <button onClick={handleSaveClick}>Save</button>
+    </div>
+  );
+};
+
+export default EditContacts;
